4cac64e5b1f6d277c4e3727cccf302cfecf2ee62
anluu203 <luutruongan08082003@gmail.com>
2024-04-16 08:46:13 +0700

    project

---
+++ js/Trangchu.js
@@ -0,0 +1,49 @@
+let list = document.querySelector('.slider .list-img');
+let images = document.querySelectorAll('.slider .list-img #image');
+let dots = document.querySelectorAll('.slider .dots li');
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+
+let active = 0;
+let lengImages = images.length-1;
+
+next.onclick = function()
+{
+    if (active + 1 > lengImages) {
+        active = 0;
+    }
+    else
+    {
+          active += 1;
+    } 
+    reloadSlider();
+}
+
+prev.onclick = function()
+{
+    if (active - 1 < 0) {
+        active = lengImages;
+    }
+    else
+    {
+          active = active - 1;
+    }  
+    reloadSlider();
+}
+function reloadSlider() 
+{
+    let checkleft = images[active].offsetLeft;
+    list.style.left = -checkleft + 'px';
+
+    let lastActiveDot = document.querySelector('.slider .dots li.active');
+    lastActiveDot.classList.remove('active');
+    dots[active].classList.add('active');
+}
+dots.forEach ((li,key) => 
+{
+    li.addEventListener('click',function () 
+    {
+        active = key;
+        reloadSlider();
+    })
+})
