db17dbc405b39cd5ad3f77572580e7525471bdfa
anluu203 <151427161+anluu203@users.noreply.github.com>
2024-04-28 23:22:53 +0700

    add

--- js/Trangchu.js
+++ js/Trangchu.js
@@ -5,45 +5,48 @@ let prev = document.getElementById('prev');
 let next = document.getElementById('next');
 
 let active = 0;
-let lengImages = images.length-1;
+let lengImages = images.length - 1;
 
-next.onclick = function()
-{
+next.onclick = function() {
     if (active + 1 > lengImages) {
         active = 0;
+    } else {
+        active += 1;
     }
-    else
-    {
-          active += 1;
-    } 
     reloadSlider();
-}
+};
 
-prev.onclick = function()
-{
+prev.onclick = function() {
     if (active - 1 < 0) {
         active = lengImages;
+    } else {
+        active -= 1;
     }
-    else
-    {
-          active = active - 1;
-    }  
     reloadSlider();
-}
-function reloadSlider() 
-{
+};
+
+let refloatslider = setInterval(() => {
+    next.click();
+}, 3000);
+
+function reloadSlider() {
     let checkleft = images[active].offsetLeft;
     list.style.left = -checkleft + 'px';
 
     let lastActiveDot = document.querySelector('.slider .dots li.active');
-    lastActiveDot.classList.remove('active');
+    if (lastActiveDot) {
+        lastActiveDot.classList.remove('active');
+    }
     dots[active].classList.add('active');
+    clearInterval(refloatslider);
+    refloatslider = setInterval(() => {
+        next.click();
+    }, 3000);
 }
-dots.forEach ((li,key) => 
-{
-    li.addEventListener('click',function () 
-    {
+
+dots.forEach((li, key) => {
+    li.addEventListener('click', function() {
         active = key;
         reloadSlider();
-    })
-})
+    });
+});
